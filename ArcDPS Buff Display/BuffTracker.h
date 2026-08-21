3520d32750d5687d74db8827108fe1d820866a60
Yangming <www.892664248@qq.com>
2026-08-21 10:29:48 +0800

    fix: update buff tracker timing interface

--- ArcDPS Buff Display/BuffTracker.h
+++ ArcDPS Buff Display/BuffTracker.h
@@ -15,7 +15,7 @@ struct ActiveEffect {
 };
 class BuffTracker {
 public:
-	void Process(cbtevent* event, ag* source, ag* destination);
+	void Process(cbtevent* event, ag* source, ag* destination, uint64_t now);
 	std::optional<ActiveEffect> Snapshot(uint32_t effect_id, EffectStacking stacking, uint64_t now);
 	void Clear();
 
@@ -23,6 +23,7 @@ private:
 	struct StackState {
 		uint32_t duration_ms = 0;
 		uint64_t end_time = 0;
+		uint64_t activation_order = 0;
 		bool active = false;
 	};
 
@@ -34,13 +35,15 @@ private:
 	std::unordered_map<uint32_t, EffectState> effects_;
 	uintptr_t self_agent_ = 0;
 	uint32_t synthetic_stack_id_ = 1;
+	uint64_t next_activation_order_ = 1;
 
 	static uint32_t StackId(const cbtevent& event);
+	static uint64_t NormalizeEventTime(uint64_t event_time, uint64_t now);
 	bool IsSelf(const ag* agent, uintptr_t event_agent) const;
-	void Apply(const cbtevent& event);
-	void Change(const cbtevent& event);
+	void Apply(const cbtevent& event, uint64_t event_time);
+	void Change(const cbtevent& event, uint64_t event_time);
 	void RemoveSingle(const cbtevent& event);
 	void RemoveAll(const cbtevent& event);
-	void ActivateStack(const cbtevent& event);
+	void ActivateStack(const cbtevent& event, uint64_t event_time);
 	void DeactivateStack(const cbtevent& event);
 };
